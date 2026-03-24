bb08b0bbc5928bf993d613f84ce0e9370003ec5f
Matteo Frigieri <matteo.frigieri@rawpowergroup.it>
2026-03-24 10:57:34 +0100

    Fix race condition on socket with asyncio.Lock + added a reduced polling interval (2secs), commented code use, can't find instructions on how to configure it.

--- custom_components/ialarm_controller/coordinator.py
+++ custom_components/ialarm_controller/coordinator.py
@@ -1,11 +1,12 @@
 """Coordinator for the iAlarm integration."""
 
 from __future__ import annotations
+from datetime import timedelta
 
 import logging
+import asyncio
 
 from homeassistant.components.alarm_control_panel import (
-    SCAN_INTERVAL,
     AlarmControlPanelState,
 )
 from homeassistant.core import HomeAssistant, ServiceResponse
@@ -17,6 +18,7 @@ from .const import DOMAIN, IALARM_TO_HASS, SERVICE_GET_LOG_MAX_ENTRIES, IAlarmSt
 
 _LOGGER = logging.getLogger(__name__)
 
+POLL_INTERVAL = timedelta(seconds=2)
 
 class IAlarmCoordinator(DataUpdateCoordinator[IAlarmStatusType]):
     """Class to manage fetching iAlarm data."""
@@ -30,89 +32,92 @@ class IAlarmCoordinator(DataUpdateCoordinator[IAlarmStatusType]):
         self.host: str = device.host
         self.mac = mac
         self.send_events = send_events
+        self._io_lock = asyncio.Lock()
 
         super().__init__(
             hass,
             _LOGGER,
             name=DOMAIN,
-            update_interval=SCAN_INTERVAL,
+            update_interval=POLL_INTERVAL,
         )
 
     async def async_cancel_alarm(self) -> None:
         """Cancel alarm alerts."""
-        await self.ialarm_device.cancel_alarm()
-        if self.send_events:
-            self.hass.bus.async_fire(event_type="cancel_alarm")
+        async with self._io_lock:
+            await self.ialarm_device.cancel_alarm()
+            if self.send_events:
+                self.hass.bus.async_fire(event_type="cancel_alarm")
 
     async def async_get_log(
         self, max_entries: int = SERVICE_GET_LOG_MAX_ENTRIES
     ) -> ServiceResponse:
         """Retrieve last n log entries."""
+        async with self._io_lock:
+            _LOGGER.debug("Retrieve last %s log entries.", max_entries)
 
-        _LOGGER.debug("Retrieve last %s log entries.", max_entries)
+            items: list[LogEntryType] = await self.ialarm_device.get_last_log_entries(
+                max_entries
+            )
 
-        items: list[LogEntryType] = await self.ialarm_device.get_last_log_entries(
-            max_entries
-        )
-
-        if items:
-            self.hass.bus.async_fire(event_type="ialarm_logs", event_data=items)
-            return {
-                "items": [
-                    {
-                        "time": item["time"],
-                        "area": item["area"],
-                        "event": item["event"],
-                        "name": item["name"],
-                    }
-                    for item in items
-                ],
-            }
-        return []
+            if items:
+                self.hass.bus.async_fire(event_type="ialarm_logs", event_data=items)
+                return {
+                    "items": [
+                        {
+                            "time": item["time"],
+                            "area": item["area"],
+                            "event": item["event"],
+                            "name": item["name"],
+                        }
+                        for item in items
+                    ],
+                }
+            return []
 
     async def _async_update_data(self) -> IAlarmStatusType:
         """Fetch data from iAlarm."""
-        try:
-            zone_status: list[
-                ZoneStatusType
-            ] = await self.ialarm_device.get_zone_status()
-            internal_alarm_status: AlarmStatusType = (
-                await self.ialarm_device.get_status(zone_status)
-            )
-
-            alarm_status_value = IALARM_TO_HASS.get(
-                internal_alarm_status["status_value"]
-            )
-
-            _LOGGER.debug("iAlarm status [%s]", alarm_status_value)
-
-            if (
-                alarm_status_value == AlarmControlPanelState.TRIGGERED
-                and self.send_events
-            ):
-                ialarm_alarmed_zones: list[ZoneStatusType] | None = (
-                    internal_alarm_status["alarmed_zones"]
+        async with self._io_lock:
+            try:
+                zone_status: list[
+                    ZoneStatusType
+                ] = await self.ialarm_device.get_zone_status()
+                internal_alarm_status: AlarmStatusType = (
+                    await self.ialarm_device.get_status(zone_status)
                 )
 
-                _LOGGER.debug(
-                    "iAlarm in TRIGGERED status with allarmed zones [%s]",
-                    ialarm_alarmed_zones,
+                alarm_status_value = IALARM_TO_HASS.get(
+                    internal_alarm_status["status_value"]
                 )
 
-                self.hass.bus.async_fire(
-                    event_type="ialarm_triggered",
-                    event_data={
-                        "alarm_status": "TRIGGERED",
-                        "alarmed_zones": ialarm_alarmed_zones,
-                    },
+                _LOGGER.debug("iAlarm status [%s]", alarm_status_value)
+
+                if (
+                    alarm_status_value == AlarmControlPanelState.TRIGGERED
+                    and self.send_events
+                ):
+                    ialarm_alarmed_zones: list[ZoneStatusType] | None = (
+                        internal_alarm_status["alarmed_zones"]
+                    )
+
+                    _LOGGER.debug(
+                        "iAlarm in TRIGGERED status with allarmed zones [%s]",
+                        ialarm_alarmed_zones,
+                    )
+
+                    self.hass.bus.async_fire(
+                        event_type="ialarm_triggered",
+                        event_data={
+                            "alarm_status": "TRIGGERED",
+                            "alarmed_zones": ialarm_alarmed_zones,
+                        },
+                    )
+
+                ialarm_status: IAlarmStatusType = IAlarmStatusType(
+                    ialarm_status=alarm_status_value, zone_status_list=zone_status
                 )
 
-            ialarm_status: IAlarmStatusType = IAlarmStatusType(
-                ialarm_status=alarm_status_value, zone_status_list=zone_status
-            )
-
-            self.state = ialarm_status
-            self.async_set_updated_data(ialarm_status)
-        except ConnectionError as error:
-            raise UpdateFailed(error) from error
-        return ialarm_status
+                self.state = ialarm_status
+                self.async_set_updated_data(ialarm_status)
+            except ConnectionError as error:
+                raise UpdateFailed(error) from error
+            return ialarm_status
